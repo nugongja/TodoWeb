dc558628b36f6601fd60db38ecb1fb348323c491
nugongja <nugongja@gmail.com>
2025-02-07 23:30:09 +0900

    Initial commit

---
+++ src/test/java/TODO/TodoWeb/TodoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package TODO.TodoWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
